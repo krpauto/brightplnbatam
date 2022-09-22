eb9978b6f99fe1e843525423036d9fa9d4de8acb
Kevin Rivaldo Panggabean <82790760+krpauto@users.noreply.github.com>
2022-09-22 10:40:16 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,69 @@
+<!--================Home Banner Area =================-->
+<section class="home_banner_area">
+	<div class="banner_inner d-flex align-items-center">
+		<div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background="">
+		</div>
+		<div class="container">
+			<div class="banner_content text-center">
+				<h3 data-aos="fade-up" data-aos-duration="1600">Bright People Bright Future <br/>
+				<p data-aos="fade-up" data-aos-duration="1900"> </p>
+				<a data-aos="fade-up" data-aos-duration="2000" class="main_btn" href="<?= base_url('user/registration') ?>#registration">Bergabung Sekarang <span class="lnr lnr-arrow-right"></span></a>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- ================End Home Banner Area ================= -->
+
+<!--================Finance Area =================-->
+<section class="finance_area">
+	<div class="container">
+		<div class="finance_inner row">
+			<div class="col-lg-3 col-sm-6">
+				<div class="finance_item">
+					<div data-aos="fade-right" data-aos-duration="1600" class="media">
+						<div class="d-flex">
+							<i class="lnr lnr-users"></i>
+						</div>
+						<div class="media-body">
+							<h5>Admin & Pelanggan</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-sm-6">
+				<div class="finance_item">
+					<div data-aos="fade-right" data-aos-duration="1800" class="media">
+						<div class="d-flex">
+							<i class="lnr lnr-file-empty"></i>
+						</div>
+						<div class="media-body">
+							<h5>Data Management Pelanggan</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+	</div>
+</section>
+<!--================End Finance Area =================-->
+
+<!--================ Illustrations Area =================-->
+<section class="learnify-for-indonesia p_20">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 mx-auto">
+				<img data-aos="fade-up" data-aos-duration="1800" src="<?= base_url('assets/') ?>img/illustrations/ alt="" srcset="">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-7 mx-auto">
+				<div class="main_title">
+					<h2 data-aos="fade-up" data-aos-duration="2000">WELCOME BRIGHT PLN BATAM</h2>
+					<p data-aos="fade-up" data-aos-duration="2200">Merciful revaluation burying love ultimate value inexpedient ubermensch. Holiest madness victorious morality hope endless christian madness. Love dead fearful transvaluation marvelous. Oneself right ideal abstract endless faith deceptions zarathustra grandeur law ubermensch free.</p>
+					<a href="http://localhost/learnify/welcome/admin"><button data-aos="fade-up" data-aos-duration="2400" class="bubbly-button">Login Admin <span class="lnr lnr-arrow-right"></span></button></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--================End Illustrations Area =================-->
+
